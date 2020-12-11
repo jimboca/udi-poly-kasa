2fa87570eaabd7246dff7975e06c959244e6d16c
JimBo CA <jimboca3@gmail.com>
2020-12-10 16:52:46 -0800

    2.4.7 Check python version on startup

--- kasa-poly.py
+++ kasa-poly.py
@@ -4,8 +4,8 @@ This is a TP-Link Kasa NodeServer for Polyglot v2 written in Python3
 by JimBo jimboca3@gmail.com
 """
 
-import polyinterface
 import sys
+import polyinterface
 import time
 import warnings
 from nodes import Controller
@@ -15,6 +15,9 @@ LOGGER = polyinterface.LOGGER
 if __name__ == "__main__":
     # Some are getting unclosed socket warnings due to garbage collection?? no idea why, so just ignore them since we dont' care
     warnings.filterwarnings("ignore", category=ResourceWarning, message="unclosed.*<socket.socket.*>")
+    if sys.version_info < (3, 6):
+        LOGGER.error("ERROR: Python 3.6 or greater is required not {}.{}".format(sys.version_info[0],sys.version_info[1]))
+        sys.exit(1)
     try:
         polyglot = polyinterface.Interface('Kasa')
         """
